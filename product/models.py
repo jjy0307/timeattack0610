34cf7ae78127d53a29de6aaa4b88a9bbc08b07ad
jjy0307 <adz7546@gmail.com>
2022-06-10 16:31:53 +0900

    add

---
+++ product/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class UserModel(models.Model):
+    class Meta:
+        db_table = "Product"
+
+    username = models.CharField(max_length=20, null=False)
+    password = models.CharField(max_length=256, null=False)
